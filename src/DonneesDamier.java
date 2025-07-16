0dc96a1f1218f344c2ddbdcb81e22d1f3d9533a9
Ami Jega <ami.jegathasan@gmail.com>
2025-07-16 12:08:12 +0530

    DonneesDamier.java

---
+++ src/DonneesDamier.java
@@ -0,0 +1,9 @@
+// === Classe DonneesDamier ===
+public interface DonneesDamier { 
+    // Constantes, code ASCII des colonnes ‘a’ à ‘h’
+    int[] Valeurs = {97, 98, 99, 100, 101, 102, 103, 104};
+    
+    //Libellés des colonnes sous formes de caractères
+    char[] Libelles = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+}//fin class DonneesDamier
