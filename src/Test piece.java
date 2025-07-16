d52ea3608af1dfc5ce63b08d2536beee5fbf6b25
Ami Jega <ami.jegathasan@gmail.com>
2025-07-16 12:13:35 +0530

    Test piece.java

---
+++ src/Test piece.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TestPiece {
+    public static void main(String[] args) {
+		/*
+    	Case c = new Case(4,"e");
+    	Case c2 = new Case(5,"e");
+
+		System.out.println(c);
+
+    	Piece t = new Tour("blanc",null);
+		Piece t2 = new Tour("noir",null);
+
+		c.setPiece(t);
+		
+		c2.setPiece(t);
+
+		System.out.println(c.getPiece());  //la tour est sur c
+		
+		System.out.println(t.getPosition()); //la tour est placé nul part
+
+		System.out.println(c2.getPiece());  // la tour est sur c2   PAS BON!
+
+
+*/
+		
+
+		String testData = "2a 3a";
+		Scanner scanner = new Scanner(System.in);
+
+		int a = scanner.nextInt();
+		System.out.println(a);
+		 
+    }
+}
